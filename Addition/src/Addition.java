bfc028e600835dca2c6ea62173e6902593c712b6
dnyaneshs1 <iamdsarode@gmail.com>
2024-09-04 16:39:48 +0530

    Addition program

---
+++ Addition/src/Addition.java
@@ -0,0 +1,10 @@
+
+
+public class Addition {
+    public static void main(String[] args) {
+        String firstString="Pune";
+        String secondString="Ahmednagar";
+        System.out.println("Concatenation  " + (firstString+ secondString));
+
+    }
+}
