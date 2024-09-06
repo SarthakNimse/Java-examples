b66e2dde9e1039b5255262e2abc8d991d87ef996
dnyaneshs1 <iamdsarode@gmail.com>
2024-09-06 14:29:39 +0530

    WhileCondition

---
+++ WhileCondition/src/WhileCondition.java
@@ -0,0 +1,11 @@
+public class WhileCondition{
+    public static void main(String[] args) {
+
+
+        int a = 1;
+        while (a <= 100) {
+            System.out.println(a);
+            a = a + 1;
+        }
+    }
+}
